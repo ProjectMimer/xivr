9b07ccc93e94012cf94d2ea25e1cdad998b912e0
ProjectMimer <117416504+ProjectMimer@users.noreply.github.com>
2022-11-11 08:24:46 +1030

    Add files via upload
    new compile script

--- dip17build.sh
+++ dip17build.sh
@@ -4,12 +4,17 @@ set -e
 
 cd xivr_main
 
+mkdir /needs/openvr_local
+7za x -y -o/needs/openvr_local -tzip /needs/openvr.zip
+
+OPENVR_VERSION="1.23.7"
+OPENVR_LOCAL="/needs/openvr_local/openvr-$OPENVR_VERSION"
 LINKER_OBJS=""
 
 compile () {
 	/opt/msvc/bin/x64/cl /permissive- /ifcOutput "x64\Release\\" /GS /GL /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /sdl /Fd"x64\Release\vc143.pdb" /Zc:inline \
 	/fp:precise /D "NDEBUG" /D "XIVRMAIN_EXPORTS" /D "_WINDOWS" /D "_USRDLL" /D "_WINDLL" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope \
-	/Gd /Oi /MD /FC /Fa"x64\Release\\" /EHsc /nologo /Fo"x64\Release\\" /Fp"x64\Release\xivr_main.pch" /diagnostics:column /I"$OPENVR_HEADERS" /c $1
+	/Gd /Oi /MD /FC /Fa"x64\Release\\" /EHsc /nologo /Fo"x64\Release\\" /Fp"x64\Release\xivr_main.pch" /diagnostics:column /I"$OPENVR_LOCAL/headers" /c $1
 	LINKER_OBJS="$LINKER_OBJS x64\Release\\${1/cpp/obj}"
 }
 
@@ -22,5 +27,9 @@ echo $LINKER_OBJS
 	/DYNAMICBASE "d3dcompiler.lib" "d3d11.lib" "dxgi.lib" "openvr_api.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" \
 	"advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /IMPLIB:"x64\Release\xivr_main.lib" /DEBUG \
 	/DLL /MACHINE:X64 /OPT:REF /SUBSYSTEM:WINDOWS /MANIFESTUAC:NO /ManifestFile:"x64\Release\xivr_main.dll.intermediate.manifest" \
-	/LTCGOUT:"x64\Release\xivr_main.iobj" /OPT:ICF /ERRORREPORT:PROMPT /ILK:"x64\Release\xivr_main.ilk" /NOLOGO /LIBPATH:"/app/xivr/openvr/lib/win64" \
+	/LTCGOUT:"x64\Release\xivr_main.iobj" /OPT:ICF /ERRORREPORT:PROMPT /ILK:"x64\Release\xivr_main.ilk" /NOLOGO /LIBPATH:"$OPENVR_LOCAL/lib/win64" \
 	/TLBID:1 $LINKER_OBJS
+
+cp x64/Release/xivr_main.dll ../xivr
+cp x64/Release/xivr_main.pdb ../xivr
+cp $OPENVR_LOCAL/bin/win64/openvr_api.dll ../xivr
